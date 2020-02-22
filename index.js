44d6f68d3e92385cb1264cdeb9faa0aafcdb7c14
Brittany Russell <sincerelybr@gmail.com>
2020-02-22 21:01:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,45 @@
+// 1) Objects
+//       updateObjectWithKeyAndValue(object, key, value)
+//         returns an object with the orignal key value pairs and the new key value pair:
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object =[key,value];
+  return object
+}
+
+//   2) Objects
+//       updateObjectWithKeyAndValue(object, key, value)
+//         does not modify the original object, but rather returns a clone with the new data:
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+
+//   3) Objects
+//       updateObjectWithKeyAndValue(object, key, value)
+//         returns an object with an updated key value pair:
+
+//   4) Objects
+//       destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+//         updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object:
+
+
+//   5) Objects
+//       deleteFromObjectByKey(object, key)
+//         deletes `key` from a clone of object and returns the new object (it isnon-destructive):
+
+
+//   6) Objects
+//       deleteFromObjectByKey(object, key)
+//         does not modify the original object (it is non-destructive):
+
+
+//   7) Objects
+//       destructivelyDeleteFromObjectByKey(object, key)
+//         returns object without the delete key/value pair:
+
+
+//   8) Objects
+//       destructivelyDeleteFromObjectByKey(object, key)
+//         modifies the original object:
