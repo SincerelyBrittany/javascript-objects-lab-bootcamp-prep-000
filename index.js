bcb71c99146da419315643c2efb7965d43148c4f
Brittany Russell <sincerelybr@gmail.com>
2020-02-22 22:06:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,9 @@
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object =[key,value];
-  return object
+  // object =[key,value];
+  // return object
+  return Object.assign({}, object, { [key]: value })
 }
 
 
